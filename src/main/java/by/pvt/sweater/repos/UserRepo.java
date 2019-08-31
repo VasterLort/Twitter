816eb9d308657509ca0310c6ee31900650581aea
Vakar Andrei <mr.vakar2016@gmail.com>
2019-08-31 13:21:49 +0300

    First commit

---
+++ src/main/java/by/pvt/sweater/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package by.pvt.sweater.repos;
+
+import by.pvt.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByActivationCode(String code);
+}
---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,12 @@
+insert into usr (id, username, password, active, email)
+values(
+   1,
+   'admin',
+   '$2a$08$IJIxzOQ3s5Ihxz9LlAQXx.R4OfwgVzrmM8Daj6zH3t5S0ULA9TtG6',
+   true,
+   'vakar057@gmail.com'
+);
+
+insert into user_role (user_id, roles)
+values(1, 'USER'),
+      (1, 'ADMIN');
